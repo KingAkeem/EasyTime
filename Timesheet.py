4a3da2da082011512df3efea04af73eb61154f4d
atking1 <akeemtlking@gmail.com>
2017-07-30 16:46:55 -0400

    Form is receiving user inputted values for username and password for testing purposes

--- Timesheet.py
+++ Timesheet.py
@@ -3,7 +3,14 @@ from robobrowser import RoboBrowser
 browser = RoboBrowser(history=True) # Creates an instance of a RoboBrowser object
 
 browser.open("https://www.coastal.edu/scs/employee") # Opens RoboBrowser to Login page
-form = browser.find_all()
+form = browser.get_form()
 
-print(form)
+username = input("What is your username? ")
+password = input("What is your password? ")
+
+form['uname'] = username
+form['pnum'] = password
+
+browser.submit_form(form)
+
 
