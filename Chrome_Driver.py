53a13385eede52b3a5f12a9087a3b90a5476b915
KingAkeem <atking1@g.coastal.edu>
2017-08-09 03:03:42 -0400

    Adding ability to find chromedriver on Linux

--- Chrome_Driver.py
+++ Chrome_Driver.py
@@ -1,12 +1,16 @@
 import scandir
-import os
+from glob import glob
 from os.path import join
 from sys import platform
 
 class Chrome_Driver:
 
     def __init__(self):
-        self.driver = 'chromedriver.exe'
+        if platform == 'win32':
+            self.driver = 'chromedriver.exe'
+
+        elif platform == 'linux':
+            self.driver = 'chromedriver'
 
     def find_path(self):
         if platform == 'win32':
@@ -15,19 +19,14 @@ class Chrome_Driver:
                 print('searching:', root)
                 if self.driver in files:
                     driver_path = join(root, self.driver)
-                    return driver_path
-
-        if platform == 'linux':
-            for root, dirs, files in os.walk("/home/atking1/Downloads/"):
-                print('searching:', root)
-                if self.driver in files:
-                    driver_path = join(root, self.driver)
                     print('found:',driver_path)
                     return driver_path
 
-
-
-
-
-
+        if platform == 'linux':
+            for root, dirs, files in scandir.walk("/home"):
+                print('searching:', root)
+                if self.driver in files or self.driver in root:
+                    driver_path = join(root, self.driver)
+                    print('found:',driver_path)
+                    return driver_path
 
