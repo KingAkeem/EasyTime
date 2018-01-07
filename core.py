dcb59f6ae94acfed424a8f495e4aef8a5b053801
Akeem King <akeemtlking@gmail.com>
2018-01-06 20:30:35 -0500

    Removed useless import

--- core.py
+++ core.py
@@ -19,7 +19,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from sys import argv
 
 
 class AutomateLogging:
